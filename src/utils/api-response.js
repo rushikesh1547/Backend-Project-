655d97b40df66eb2890d00ee8ff1cd76e6a1cc41
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2025-12-27 14:32:48 +0530

    api response and error

---
+++ src/utils/api-response.js
@@ -0,0 +1,9 @@
+class ApiResponse{
+    constructor(statusCode, data, message = "Success"){
+        this.statuscode = statusCode
+        this.message = message
+        this.success = statusCode < 400;   
+    }
+}
+
+export{ApiResponse};
